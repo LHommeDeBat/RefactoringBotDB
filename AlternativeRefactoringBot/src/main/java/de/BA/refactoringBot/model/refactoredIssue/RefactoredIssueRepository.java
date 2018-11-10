462b6e1fdd87d611b909ee32ea62cee7e7381078
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2018-11-10 09:42:48 +0100

    REST-Controller für bearbeitete Issues hinzugefügt

--- AlternativeRefactoringBot/src/main/java/de/BA/refactoringBot/model/refactoredIssue/RefactoredIssueRepository.java
+++ AlternativeRefactoringBot/src/main/java/de/BA/refactoringBot/model/refactoredIssue/RefactoredIssueRepository.java
@@ -2,9 +2,18 @@ package de.BA.refactoringBot.model.refactoredIssue;
 
 import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 @Transactional
 public interface RefactoredIssueRepository extends CrudRepository<RefactoredIssue, Long> {
+
+	@Query("SELECT a FROM RefactoredIssue a WHERE a.repoService=:repoService")
+	public Iterable<RefactoredIssue> getAllServiceRefactorings(@Param("repoService") String repoService);
+
+	@Query("SELECT a FROM RefactoredIssue a WHERE a.repoService=:repoService and a.repoOwner=:repoOwner")
+	public Iterable<RefactoredIssue> getAllUserIssues(@Param("repoService") String repoService,
+			@Param("repoOwner") String repoOwner);
 
 }
