c66722450c420a39b60c728f7adc3334a84b9600
Justin Kissling <st107873@stud.uni-stuttgart.de>
2018-12-03 13:22:43 +0100

    Now properly removes comments while keeping formatting

--- AlternativeRefactoringBot/src/main/java/de/BA/refactoringBot/refactoring/supportedRefactorings/RemoveCommentedOutCode.java
+++ AlternativeRefactoringBot/src/main/java/de/BA/refactoringBot/refactoring/supportedRefactorings/RemoveCommentedOutCode.java
@@ -1,15 +1,13 @@
 package de.BA.refactoringBot.refactoring.supportedRefactorings;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.github.javaparser.JavaParser;
+import com.github.javaparser.Range;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.comments.Comment;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter;
@@ -17,9 +15,14 @@ import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinte
 import de.BA.refactoringBot.configuration.BotConfiguration;
 import de.BA.refactoringBot.model.botIssue.BotIssue;
 import de.BA.refactoringBot.model.configuration.GitConfiguration;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * This class is used for executing the removal of commented out code
@@ -35,38 +38,62 @@ public class RemoveCommentedOutCode extends VoidVisitorAdapter<Object> {
     BotConfiguration botConfig;
 
     /**
-     * This method performs the refactoring and returns the a commit message.
+     * This method performs the refactoring and returns a commit message.
      *
      * @param issue
      * @param gitConfig
      * @return commitMessage
      * @throws FileNotFoundException
      */
-    public String performRefactoring(BotIssue issue, GitConfiguration gitConfig) throws FileNotFoundException {
+    public String performRefactoring(BotIssue issue, GitConfiguration gitConfig) throws FileNotFoundException, IOException {
         // Prepare data
-        String path = issue.getFilePath();
+        String path
+                = botConfig.getBotRefactoringDirectory() + gitConfig.getConfigurationId() + "/" + issue.getFilePath();
         line = issue.getLine();
 
-        CompilationUnit compilationUnit = JavaParser.parse(new File(botConfig.getBotRefactoringDirectory() + gitConfig.getConfigurationId() + "/" + path));
+        // Read file
+        FileInputStream in = new FileInputStream(path);
+        CompilationUnit compilationUnit = LexicalPreservingPrinter.setup(JavaParser.parse(in));
 
         List<Comment> comments = compilationUnit.getAllContainedComments();
 
-        for (Comment co : comments) {
-            System.out.println(co.getRange().get().begin.line);
-            if (co.getRange().get().begin.line == line) {
-                System.out.println(co.getContent());
-                co.remove();
+        for (Comment comment : comments) {
+            if ((line >= comment.getBegin().get().line) && (line <= comment.getEnd().get().line)) {
+                System.out.println("Removing comment");
+                removeLinesFromFile(comment.getRange().get(), path);
+                break;
             }
         }
 
-        // Save changes to file
-        PrintWriter out = new PrintWriter(
-                botConfig.getBotRefactoringDirectory() + gitConfig.getConfigurationId() + "/" + path);
-        out.println(LexicalPreservingPrinter.print(compilationUnit));
-        out.close();
-
         // Return commit message
-        return "Removed commented out code at";
+        return "Removed commented out code at line " + line;
+    }
+
+    private void removeLinesFromFile(Range lines, String path) throws FileNotFoundException, IOException {
+        File inputFile = new File(path);
+        File tempFile = new File(inputFile.getParent() + File.separator + "temp.java");
+        System.out.println(inputFile.getParent() + File.separator + "temp.java");
+
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+        String currentLine;
+        int lineNumber = 0;
+
+        while ((currentLine = reader.readLine()) != null) {
+            lineNumber++;
+
+            if ((lineNumber >= lines.begin.line) && (lineNumber <= lines.end.line)) {
+                continue;
+            }
+            writer.write(currentLine + System.getProperty("line.separator"));
+        }
+        
+        writer.close();
+        reader.close();
+        inputFile.delete();
+        boolean successful = tempFile.renameTo(inputFile);
+
     }
 
 }
